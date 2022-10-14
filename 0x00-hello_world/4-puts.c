6206e93e7272c14ea0200753c7c66782ded56920
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-13 21:19:11 -0700

    THe entire work done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- serves as the main entry point for your program
+ * Return: zero if no errors otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- is the entry point for the program.
+ * Return: zero if no error, and non zero if there is error
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

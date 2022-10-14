705fd621454754584ab832efda8e5bfeb2e3f675
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-13 21:49:35 -0700

    Final corrective work done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * main- is the main program entry point.
- * Return: 1 when it's successful.
+ * Return: one when it's successful
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

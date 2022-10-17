31c73a73cc7a2acc3342412cb714a2a48552edbf
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-17 08:25:46 -0700

    every

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int d, g;
 
-	for (d = 0; d < 9; d++)
+	for (d = 48; d < 56; d++)
 	{
-		for (g = d + 1; g < 10; g++)
+		for (g = 49; g < 57; g++)
 		{
 			if (d > g)
 			{
 				putchar((d % 10) + '0');
 				putchar((g % 10) + '0');
-				if (d != 8 || g != 9)
+				if (d != 56 || g != 57)
 				{
 					putchar(',');
 					putchar(' ');
@@ -27,5 +27,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }

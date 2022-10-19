3c84785261bb21f626922d7fb6e28a732c664c56
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-19 08:35:05 -0700

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,25 +6,25 @@
 
 void times_table(void)
 {
-	int num, multiply, product;
+	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (multiply = 1; multiply <= 9; multiply++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar('-');
 			_putchar(' ');
 
-			product = num * multiply;
+			product = num * mult;
 
-			if (product <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((product / 10) + '0');
+				_putchar((prod / 10) + '0');
 
-			_putchar((product % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}

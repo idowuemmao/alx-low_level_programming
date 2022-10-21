06ea2ee87fcff709175fd3108862fba459d55fa6
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-20 19:43:54 -0700

    work

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - used to test function that prints if integer is positive
+ *
+ * done by emmygreat
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+
+	j = 0;
+
+	positive_or_negative(j);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Done by emmygreat
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int w;
+
+	printf("Infinite loop incoming: (\n");
+
+	w = 0;
+
+	/* while (w < 10)*/
+	/* {*/
+	/* putchar(w);*/
+	/* }*/
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}

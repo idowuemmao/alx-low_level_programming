bfacb8837346984c19356f7c7f4edde061d5d4ba
Idowu Emmanuel <idowuemmao@gmail.com>
2022-10-27 06:31:23 -0700

    great

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,12 +8,14 @@
 
 int main(void)
 {
-	int m;
+	int n;
 	int a[5];
 	int *p;
 
 	a[2] = 1024;
-	p = &m;
+	p = &n;
+
+
 
 	*(p + 5) = 98;
 

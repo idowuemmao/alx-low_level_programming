aeada6a6022923472c0c6737fd57d97637bf631e
Idowu Emmanuel <idowuemmao@gmail.com>
2022-11-03 22:22:16 -0700

    entire work

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,4 +19,19 @@ int str_checker(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, J + 1));
 	if (s2[j] == '*')
-		return (str_checker(s1, s2, i + 1, j)) ||
+		return (str_checker(s1, s2, i + 1, j)) || str_checker(s1, s2, i, j + 1);
+	return (0);
+}
+
+/**
+ * wildcmp - check if string could be considered identical
+ * @s1: base address
+ * @s2: base address
+ *
+ * Return: 1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0));
+}

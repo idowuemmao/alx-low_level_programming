be71f8567efee94eb8e00e50738d641547b24d97
Idowu Emmanuel <idowuemmao@gmail.com>
2022-11-15 09:33:08 -0800

    grace

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,19 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define TEXT "Hello, Holberton"
+#endif
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * done by emmygreat
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf("%s\n", TEXT);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the file name
+ * Done by emmygreat
+ * Return: Zero
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
